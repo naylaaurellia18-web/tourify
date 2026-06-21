7f31fcc6695e68097003c2f4196579b0f524a7c6
naylaaurellia18 <naylalia556@gmail.com>
2026-06-21 15:17:38 +0700

    BISMILLAH

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,6 +1,28 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "tourify");
-if (!$conn) {
+$host     = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$port     = 4000;
+$username = "3DA4d4bPMVCSuDy.root";
+$password = "mRSgOTH6qk79AieJ";
+$database = "tourify"; // ganti dari "sys" ke nama database kamu
+
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+$connected = mysqli_real_connect(
+    $conn,
+    $host,
+    $username,
+    $password,
+    $database,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$connected) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8mb4");
 ?>
